89213c95094fc1426d72d2a57ad13ed6d68cd9bc
byanks <byanks@users.noreply.github.com>
2019-01-29 20:55:25 +0800

    Add files via upload

---
+++ Period.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeriodTracker
+{
+    class Period
+    {
+        public DateTime previousDate
+        { get; set; }
+
+        public DateTime newDate
+        { get; set; }
+
+        public DateTime lastDate
+        { get; set; }
+
+        public string Notes
+        { get; set; }
+
+        public string CalculateDays()
+        {
+            TimeSpan t = newDate - previousDate;
+            double dDays = t.TotalDays;
+            int days = Convert.ToInt32(dDays);
+            string totalDays = days.ToString();
+            return totalDays;
+        }
+
+        public string CalculateDuration()
+        {
+            TimeSpan d = lastDate - newDate;
+            double dDuration = d.TotalDays;
+            int duration = Convert.ToInt32(dDuration) + 1;
+            string totalDuration = duration.ToString();
+            return totalDuration;
+        }
+    }
+}
